02c8ce50a56c81e037ac725f46707c02693ca57c
Adam Beguelin <adam@beguelin.com>
2025-05-18 15:17:00 +0700

    update the email

--- functions/email.js
+++ functions/email.js
@@ -15,8 +15,8 @@ export async function sendEmail({ to, subject, text, html, redirectUrl, MAILHOP_
   const requestBody = {
     messages: [{
       from: { 
-        name: "code.pr",
-        email: 'noreply@code.pr' 
+        name: "selfie.pr",
+        email: 'noreply@selfie.pr' 
       },
       to: [{ 
         email: to 
@@ -50,8 +50,8 @@ export async function sendEmail({ to, subject, text, html, redirectUrl, MAILHOP_
       body: JSON.stringify({
         messages: [{
           from: { 
-            name: "code.pr",
-            email: 'noreply@code.pr' 
+            name: "selfie.pr",
+            email: 'noreply@selfie.pr' 
           },
           to: [{ 
             email: to 
